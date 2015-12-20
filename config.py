90a6c2ca0c0624acb5520648074c4c9a5a2add22
LobsterBandit <LobsterBandit@users.noreply.github.com>
2015-12-20 00:56:06 -0500

    Added PER_PAGE global for pagination

--- config.py
+++ config.py
@@ -6,3 +6,8 @@ SECRET_KEY = 'ijdhf-iaal-siej-f95-1nm-dw012'
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = os.path.join(basedir, 'Movie_DB.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(os.path.join(basedir, 'Movie_DB.db'))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+PER_PAGE = 21
