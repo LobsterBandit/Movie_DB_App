56b2ad72c63aa4241fa502a32a75e6a9d572cca6
LobsterBandit <LobsterBandit@users.noreply.github.com>
2015-12-20 00:57:19 -0500

    Added SQLAlchemy db reference

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='')
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from app import views, database
+from app import views, database, models
